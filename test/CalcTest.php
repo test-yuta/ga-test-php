7b2c6cb085ec926706a2b8b159225fcf38ec87d2
test-yuta <108449978+test-yuta@users.noreply.github.com>
2024-04-15 16:50:42 +0900

    テストファイル追加

---
+++ test/CalcTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use HogeCompany\FooProject\Calc;
+use PHPUnit\Framework\TestCase;
+
+class CalcTest extends TestCase {
+    public function testAdd() {
+        $calc = new Calc();
+        $result = $calc->add(1, 2);
+        $this->assertEquals(3, $result);
+    }
+}
